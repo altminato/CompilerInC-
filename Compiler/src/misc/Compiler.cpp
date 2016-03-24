306a3d8a5474df533bf222e0a5a5d0debabd7e1b
Antonio <altminato@gmail.com>
2016-03-24 11:22:02 +0000

    Modifying the gitignore file.

---
+++ Compiler/src/misc/Compiler.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Compiler.cpp
+// Author      : Antonio Lugo
+// Version     :
+// Copyright   : Creating a compiler
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+/*
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}*/
